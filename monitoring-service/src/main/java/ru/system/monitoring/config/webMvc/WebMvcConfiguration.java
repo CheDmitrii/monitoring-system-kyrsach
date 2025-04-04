1ac549a407bc90e94583f31fb4c3ab31c44d07c4
CheDmitrii <dima.chebykin2000@mail.ru>
2025-04-04 10:41:27 +0300

    change configurations

--- monitoring-service/src/main/java/ru/system/monitoring/config/webMvc/WebMvcConfiguration.java
+++ monitoring-service/src/main/java/ru/system/monitoring/config/webMvc/WebMvcConfiguration.java
@@ -6,12 +6,22 @@ import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableAsync
 public class WebMvcConfiguration implements WebMvcConfigurer {
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // --
+                .allowedOrigins("*")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
 
     @Bean
     public AsyncTaskExecutor asyncTaskExecutor() {
